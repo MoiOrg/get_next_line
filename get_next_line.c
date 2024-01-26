82f02e4cfc17626a51946e47dee9404810b8057c
kiparis42 <killian.ecole>
2024-01-26 16:42:31 +0100

    gnl push

--- get_next_line.c
+++ get_next_line.c
@@ -91,33 +91,33 @@ char	*read_file(int fd, char *stack)
 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (free(stack), NULL);
-	while(((count = read(fd, buffer, BUFFER_SIZE)) > 0))
+	count = read(fd, buffer, BUFFER_SIZE);
+	while ((count > 0))
 	{
 		buffer[count] = 0;
 		stack = join_free(stack, buffer);
 		if (ft_strchr(buffer, '\n'))
 			break ;
+		count = read(fd, buffer, BUFFER_SIZE);
 	}
-	free(buffer);
 	if ((count == 0 && ft_strlen(stack) == 0) || count < 0)
-		return (free(stack), NULL);
-	return (stack);
+		return (free(stack), free(buffer), NULL);
+	return (free(buffer), stack);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*stack;
 	char		*line;
-	
+
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
- 		return (free(stack), NULL);
+		return (free(stack), NULL);
 	stack = read_file(fd, stack);
 	if (!stack)
 		return (NULL);
 	line = stack_clean(stack);
 	if (!line)
 		return (NULL);
-//	printf("stack == %s\n", stack);
 	stack = stack_rest(stack);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -13,17 +13,16 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 8
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 8
-#endif
-
-
 void	ft_bzero(void *s, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*get_next_line(int	fd);
+char	*get_next_line(int fd);
 char	*ft_strchr(const char *string, int c );
 size_t	ft_strlen(const char *str);
 
